fa148da259921c5f0a8cb5c7dca39f378e37df8e
Anas Nashif <nashif@intel.com>
2007-02-15 17:16:13 +0000

    parse command line handle exception

--- examples/cpp/EnumInstance.cpp
+++ examples/cpp/EnumInstance.cpp
@@ -6,9 +6,31 @@ using std::cout;
 using namespace WsmanClientNamespace;
 int main(int argc, char* argv[])
 {
-	WsmanClient client = WsmanClient("http://wsman:secret@192.168.1.41:8889/wsman");
+	const char *endpoint, *resource_uri;
+	if (argc< 3) {
+		fprintf(stderr, "Usage: %s endpoint resource uri\n", argv[0]);
+	}
+	endpoint= argv[1];
+	resource_uri = argv[2];
+
+	if (!endpoint || !resource_uri) {
+		fprintf(stderr, "endpoint option required\n");
+		return 1;
+	}
+	WsmanClient client = WsmanClient(endpoint);
 	vector<string> vec;
-	client.Enumerate("http:///cws.sblim.sf.net/wbem/wscim/1/cim-schema/2/CWS_Instance", vec );
+	try {
+		client.Enumerate(resource_uri, vec );
+	}
+
+	catch (WsmanException &e) {
+		cout << "Fault:\n";
+		cout << "\tCode:\t\t" << e.GetFaultCode() << "\n";
+		cout << "\tSubCode:\t" << e.GetFaultSubcode() << "\n";
+		cout << "\tDetail:\t\t" << e.GetFaultDetail() << "\n";
+		cout << "\tReson:\t\t" << e.GetFaultReason() << "\n";
+
+	}
 	for (vector<string>::iterator iter = vec.begin();
 			iter != vec.end(); ++iter) {
 		cout << "item: " << *iter << "\n";
