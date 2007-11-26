0c759ef18258b8a89fedf07cf2ff6478f51020f2
Liang Hou <houliang@intel.com>
2007-11-26 05:26:36 +0000

    update for unsubscribe/renew interface change

--- src/wsman.c
+++ src/wsman.c
@@ -814,7 +814,7 @@ int main(int argc, char **argv)
 		}
 		break;
 	case WSMAN_ACTION_UNSUBSCRIBE:
-		rqstDoc = wsmc_action_unsubscribe(cl, options, event_subscription_id);
+		rqstDoc = wsmc_action_unsubscribe(cl, resource_uri, options, event_subscription_id);
 		wsman_output(cl, rqstDoc);
 		if (rqstDoc) {
 			ws_xml_destroy_doc(rqstDoc);
@@ -823,7 +823,7 @@ int main(int argc, char **argv)
 	case WSMAN_ACTION_RENEW:
 		if(event_subscription_expire)
 			options->expires = event_subscription_expire;
-		rqstDoc = wsmc_action_renew(cl, options, event_subscription_id);
+		rqstDoc = wsmc_action_renew(cl, resource_uri, options, event_subscription_id);
 		wsman_output(cl, rqstDoc);
 		if (rqstDoc) {
 			ws_xml_destroy_doc(rqstDoc);
